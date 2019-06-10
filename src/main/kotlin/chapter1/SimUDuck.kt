04891003f7cc086a1f30d23e693c0af494363120
jatzuk <jatzuk.22@gmail.com>
2019-06-10 20:41:55 +0300

    ch1

---
+++ src/main/kotlin/chapter1/SimUDuck.kt
@@ -0,0 +1,88 @@
+package chapter1
+
+/* 
+ * Created with passion and love
+ *    for project head-first-design-patterns
+ *        by Jatzuk on 10.06.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+interface FlyBehavior {
+    fun fly()
+}
+
+interface QuackBehavior {
+    fun quack()
+}
+
+abstract class Duck(var quackBehavior: QuackBehavior, var flyBehavior: FlyBehavior) {
+
+    abstract fun display()
+
+    fun performFly() {
+        flyBehavior.fly()
+    }
+
+    fun performQuack() {
+        quackBehavior.quack()
+    }
+
+    fun swim() {
+        println("All ducks float, even decoys!")
+    }
+}
+
+class FlyWithWings : FlyBehavior {
+    override fun fly() {
+        println("I'm flying!")
+    }
+}
+
+class FlyNoWay : FlyBehavior {
+    override fun fly() {
+        println("I can't fly!")
+    }
+}
+
+class Quack : QuackBehavior {
+    override fun quack() {
+        println("Quack")
+    }
+}
+
+class MuteQuack : QuackBehavior {
+    override fun quack() {
+        println("<< Silence >>")
+    }
+}
+
+class Squeak : QuackBehavior {
+    override fun quack() {
+        println("Squeak")
+    }
+}
+
+class MallardDuck : Duck(Quack(), FlyWithWings()) {
+    override fun display() {
+        println("I'm a real Mallard duck")
+    }
+}
+
+class ModelDuck : Duck(Quack(), FlyNoWay()) {
+    override fun display() {
+        println("I'am a model duck")
+    }
+}
+
+class FlyRocketPowered : FlyBehavior {
+    override fun fly() {
+        println("I'm flying with a rocket!")
+    }
+}
---
+++ src/test/kotlin/chapter1/SimUDuckTest.kt
@@ -0,0 +1,34 @@
+package chapter1
+
+import org.junit.Test
+
+
+/*
+ * Created with passion and love
+ *    for project head-first-design-patterns
+ *        by Jatzuk on 10.06.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class SimUDuckTest {
+    @Test
+    fun simUDuckTest() {
+        with(MallardDuck()) {
+            performQuack()
+            performFly()
+        }
+
+        ModelDuck().apply {
+            performFly()
+            flyBehavior = FlyRocketPowered()
+            performFly()
+        }
+    }
+}
